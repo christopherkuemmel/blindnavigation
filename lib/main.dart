10a48b73aae2cc5a9adf7266d1896b6ccca5e25e
NoraKrbr <nora@koreuber.de>
2019-04-05 12:30:56 +0200

    combine home.dart and main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'home.dart';
+import 'placeholder.dart';
+import 'camera_stream.dart';
 
 List<CameraDescription> cameras;
 
@@ -14,13 +15,57 @@ Future<Null> main() async {
   runApp(new App());
 }
 
-
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: Home(cameras),
+      home: Home(),
     );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    CameraStream(cameras),
+    PlaceholderWidget(Colors.green),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Object Detection'),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.camera_alt),
+            title: new Text('Camera'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.videocam),
+            title: new Text('Video'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
