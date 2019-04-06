dfbc9c2ae14e2b1b46471ad4b0a01824a5ed0d40
NoraKrbr <nora@koreuber.de>
2019-04-06 14:48:23 +0200

    space content in settings

--- lib/settings.dart
+++ lib/settings.dart
@@ -9,39 +9,48 @@ class _SettingsState extends State {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Settings"),
+        title: Text ('Settings'),
       ),
       body: new Container(
+          padding: new EdgeInsets.all(32.0),
           child: Column(
-        children: <Widget>[
-          Row(
             children: <Widget>[
-              Text(
-                'Resolution',
-                style: TextStyle(
-                  fontSize: 20,
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      'Resolution:',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    )
+                  ],
                 ),
-              )
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  new RaisedButton(
+                    padding: const EdgeInsets.all(8.0),
+                    onPressed: _setResolution(0),
+                    child: new Text('Low'),
+                  ),
+                  new RaisedButton(
+                    padding: const EdgeInsets.all(8.0),
+                    onPressed: _setResolution(1),
+                    child: Text('Medium'),
+                  ),
+                  new RaisedButton(
+                    padding: const EdgeInsets.all(8.0),
+                    onPressed: _setResolution(2),
+                    child: Text('High'),
+                  ),
+                ],
+              ),
             ],
-          ),
-          Row(
-            children: <Widget>[
-              new FlatButton(
-                onPressed: _setResolution(0),
-                child: Text('Low'),
-              ),
-              new FlatButton(
-                onPressed: _setResolution(1),
-                child: Text('Medium'),
-              ),
-              new FlatButton(
-                onPressed: _setResolution(2),
-                child: Text('High'),
-              ),
-            ],
-          ),
-        ],
-      )),
+          )),
     );
   }
 }
