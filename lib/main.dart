7544f4f86aebdb51dea9c270da05f76647e6d381
Christopher Kümmel <kuemmel.christopher@gmail.com>
2019-05-13 17:38:27 +0200

    add first working bluetooth example

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,54 @@
-import 'dart:async';
+// import 'dart:async';
 
-import 'package:camera/camera.dart';
+// import 'package:camera/camera.dart';
+// import 'package:flutter/material.dart';
+// import 'package:testapp/home.dart';
+
+// import 'settings.dart';
+
+// List<CameraDescription> cameras;
+
+// Future<Null> main() async {
+//   try {
+//     cameras = await availableCameras();
+//   } on CameraException catch (e) {
+//     print('Error: $e.code\nError Message: $e.message');
+//   }
+//   runApp(App(cameras: cameras));
+// }
+
+// class App extends StatelessWidget {
+//   final List<CameraDescription> cameras;
+
+//   const App({Key key, @required this.cameras}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData.dark(),
+//       home: Home(cameras: cameras),
+//       routes: <String, WidgetBuilder>{
+//         '/settings': (BuildContext context) => Settings(),
+//       },
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:testapp/home.dart';
 
-import 'settings.dart';
+import 'bluetooth.dart';
 
-List<CameraDescription> cameras;
+void main() => runApp(MyApp());
 
-Future<Null> main() async {
-  try {
-    cameras = await availableCameras();
-  } on CameraException catch (e) {
-    print('Error: $e.code\nError Message: $e.message');
-  }
-  runApp(App(cameras: cameras));
-}
-
-class App extends StatelessWidget {
-  final List<CameraDescription> cameras;
-
-  const App({Key key, @required this.cameras}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
-      home: Home(cameras: cameras),
-      routes: <String, WidgetBuilder>{
-        '/settings': (BuildContext context) => Settings(),
-      },
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BluetoothApp(), // BluetoothApp() would be defined later 
     );
   }
 }
